adff3d9d4226e40b577ec6164648bc0e6f1e681c
mgiota <giota85@gmail.com>
2021-05-20 20:43:09 +0200

    initial jest setup with babel

---
+++ filter/removeOddNumbers.js
@@ -0,0 +1,11 @@
+const removeOddNumbers = arr => {
+    return [];
+}
+
+export default removeOddNumbers;
+
+
+// var arr = [1, 2, 3, 4, 5, 6]
+// removeOddNumbers(arr) => [2, 4, 6]
+// removeOddNumbers([]) => []
+// removeOddNumbers([2, 4, 6, 8]) => [2, 4, 6, 8]
---
+++ filter/test.js
@@ -0,0 +1,10 @@
+import removeOddNumbers from './removeOddNumbers';
+
+describe('#removeDuplicates', () => {
+    it('should remove odd numbers from array', () => {
+        const result = removeOddNumbers([]);
+        console.log(result);
+        expect(1).toEqual(1);
+    });
+
+  });
